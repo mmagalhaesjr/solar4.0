e1916fbcd29c3beabff9b76deadbb8f71960c49b
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-09-22 15:57:45 -0300

    update

--- src/componentes/Mvv/Mvv.jsx
+++ src/componentes/Mvv/Mvv.jsx
@@ -2,11 +2,35 @@ import { StyledMvv } from "./styled";
 
 
 export default function Mvv() {
-    return(
-        <StyledMvv>    
-        <div id="titulo">
-            <h1>MISÃO<h2>VISÃO</h2>VALORES</h1>
-        </div>
+    return (
+        <StyledMvv>
+            <div id="container">
+                <div className="card">
+                    <h1>MISÃO</h1>
+                    <div className="texto">
+                        Prover soluções de alta qualidade em energia solar,
+                        assegurando a satisfação dos clientes e
+                        contribuindo para o desenvolvimento sustentável.
+                    </div>
+                </div>
+                <div className="card">
+                    <h1>VISÃO</h1>
+                    <div className="texto">
+                        Fortalecer a marca em qualidade, eficiência operacional e
+                        pós-venda, consolidando a empresa como referência nacional
+                        em soluções de energia solar até 2024.
+                    </div>
+                </div>
+                <div className="card">
+                    <h1>VALORES</h1>
+                    <div className="texto">
+                        Elevado padrão de ética, contínua responsabilidade corporativa, ambiente de respeito e
+                        valorização das pessoas, com a satisfação de clientes,
+                        funcionários, fornecedores e investidores.
+                    </div>
+                </div>
+
+            </div>
         </StyledMvv>
     )
 }
--- src/componentes/Mvv/styled.js
+++ src/componentes/Mvv/styled.js
@@ -9,26 +9,49 @@ position: sticky;
 top: 0;
 left: 0; */
 
+/* border: solid 3px #00BF3E; */
 background-color: #eceeee;  
 
 
-/* border: solid 3px #00BF3E; */
+#container{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  gap: 20px;
 
-display: flex;
-flex-direction: column;
-text-align: center;
+}
 
-#titulo{
-  font-size: 50px;
-  font-weight:700;
-  margin-top: 80px;
+.card{
+  width: 25%;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  border: solid 1px blue ;
+}
+.texto{
+  width: 70%;
+  height: 11rem;
+  font-size: 1.5rem;
+  margin-top: 10px;
+ 
+
+  
 }
 h1{
-  color:#000000 ;
+  color: #00BF3E;
+  font-size: 4rem;
 }
-h2{
-  color:#00BF3E ;
-}
+
+
+
+ 
+
 
 
 
