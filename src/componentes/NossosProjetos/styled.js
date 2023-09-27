38599a215af22bb7877ab334cc460b20bd5de274
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-09-27 11:42:33 -0300

    laout soluçoes

--- src/componentes/NossosProjetos/styled.js
+++ src/componentes/NossosProjetos/styled.js
@@ -23,6 +23,12 @@ text-align: center;
   font-weight:700;
   margin-top: 80px;
 }
+#titulo2{
+  font-size: 50px;
+  font-weight:700;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 h1{
   color:#000000 ;
 }
@@ -31,8 +37,37 @@ h2{
 }
 
 
+#container{
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
+#texto{
+  width: 60%;
+  font-size: 22px;
+}
 
+span{
+  font-weight: 700;
+}
+#projetos{
+  display: flex;
+  justify-content:space-evenly;
+  width: 100%;
+}
+.projetos{
+  width: 350px;
+  gap: 30px;
+  height: 250px; 
+
+  background: rgba(255, 255, 255, 0.5); 
+  backdrop-filter: blur(10px); 
+  box-shadow: 5px 5px 10px  rgba(0, 0, 0, 0.5);
+  border-radius:10px ;
+}
 
 
 
