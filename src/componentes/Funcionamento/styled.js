7cd5aa42ed1baf06c7ad16c5e0a7cacf95f73116
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-10-03 10:03:18 -0300

    teste

--- src/componentes/Funcionamento/styled.js
+++ src/componentes/Funcionamento/styled.js
@@ -37,7 +37,7 @@ h2{
 
 
 /* background-image:url('https://th.bing.com/th/id/R.42b312917c353b788856a3e9ae1df783?rik=NBuYpBBDyr5Fqg&riu=http%3a%2f%2fbluesol.com.br%2fwp-content%2fuploads%2f2016%2f11%2fimagem-2.png&ehk=ACoCTghZt6U5uPP3aG3NQrH0to5icqxizyFA10KqVqU%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1') ; */
-background-image:url("src/assets/casinha.png");
+background-image:url('/src/assets/casinha.png');
 
 background-repeat: no-repeat; 
 
