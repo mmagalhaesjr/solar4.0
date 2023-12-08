dfe39c2f5097f2cae5a7757785210980a2e69edd
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-12-08 11:30:35 -0300

    responsividade 500px

--- src/componentes/LandingPages/Topo/Pagina2/Pagina2.jsx
+++ src/componentes/LandingPages/Topo/Pagina2/Pagina2.jsx
@@ -1,5 +1,14 @@
 import { StyledPagina2 } from './styled';
 
+import gota from '../../../../assets/logosIcones/cor.png'
+
+import icone1 from '../../../../assets/orcamento/PNG/casa.png'
+import icone2 from '../../../../assets/orcamento/PNG/dinheiro.png'
+import icone3 from '../../../../assets/orcamento/PNG/ferramenta.png'
+import icone4 from '../../../../assets/orcamento/PNG/mao.png'
+import icone5 from '../../../../assets/orcamento/PNG/medalha.png'
+import icone6 from '../../../../assets/orcamento/PNG/anos.png'
+
 
 
 export default function Pagina2() {
@@ -8,9 +17,54 @@ export default function Pagina2() {
             <div className='meioVerde'>
                 <div className='cabecalho'>
                     <div className='txtTitulo' >
-
+                        <h1>Benefícios</h1>
+                        <h2>A energia solar traz diversas vantagens para você!</h2>
                     </div>
-                    <img src="" alt="" />
+                    <img src={gota} alt="" />
+                </div>
+                <div className='containerIcones'>
+
+                    <div className='cxCadaIcone'>
+                        <img src={icone1} alt="icone" />
+                        <p>
+                            Valorização do imóvel
+                        </p>
+                    </div>
+                    <div className='cxCadaIcone'>
+                        <img src={icone2} alt="icone" />
+                        <p>
+                            Economia de até 95%
+                            na conta de luz
+                        </p>
+                    </div>
+                    <div className='cxCadaIcone'>
+                        <img src={icone3} alt="icone" />
+                        <p>
+                            Assistência técnica
+                            nacional
+                        </p>
+                    </div>
+                    <div className='cxCadaIcone'>
+                        <img src={icone4} alt="icone" />
+                        <p>
+                            Retorno garantido
+                            do investimento
+                        </p>
+                    </div>
+                    <div className='cxCadaIcone'>
+                        <img src={icone5} alt="icone" />
+                        <p>
+                            Empresa reconhecida
+                            em todo o Brasil
+                        </p>
+                    </div>
+                    <div className='cxCadaIcone'>
+                        <img src={icone6} alt="icone" />
+                        <p>
+                            Duração do investimento
+                            de até 25 anos
+                        </p>
+                    </div>
                 </div>
             </div>
         </StyledPagina2>
--- src/componentes/LandingPages/Topo/Pagina2/styled.js
+++ src/componentes/LandingPages/Topo/Pagina2/styled.js
@@ -3,15 +3,11 @@ import styled from "styled-components";
 import f1 from '../../../../assets/landingPage/topoFunil/foto1.webp'
 
 
-
 const foto1 = `url(${f1})`;
 
 export const StyledPagina2 = styled.section`
-
     width: 100%;
     height: 100vh;
-    
-
     background-image: ${foto1};
     background-size: cover;
     background-repeat: no-repeat;
@@ -30,19 +26,127 @@ export const StyledPagina2 = styled.section`
         left: -15%;
         width: 70%;
         height: 100%;
-        background-color: #14a238;
+        background-color: #198f37;
         border-radius: 0px 0px 700px 0px;
         box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.8); 
+
+        /* display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center; */
     }
     .cabecalho{
         width: 100%;
-        height: 200px;
-        border: solid 2px red;
+        height: 250px;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        border: solid 2px red; 
+    }
+    .txtTitulo{
+        width: 400px;
+        margin-left: 5%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+
+        border: solid 1px #ffffff;
+    }
+    .txtTitulo h1{
+        font-size: 5rem;
+        color: #ffffff;
+        font-weight: 600;
+    }
+    .txtTitulo h2{
+        font-size: 1.5rem;
+        color: #ffffff;
+        font-weight: 600;
+        margin-top: 5%;
+    }
+    .cabecalho img{
+        width:400px;
+    }
+    .containerIcones{
+        width: 70%;
         
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5%;
+        flex-wrap: wrap;
+    }
+    .cxCadaIcone{
+        width: 200px;
+        height: 200px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+
+        border: solid 1px red;
+    }
+    .cxCadaIcone p{
+        font-size:1rem;
+        flex-wrap: wrap;
+        margin-top: 5%;
+        color: #ffffff;
+    }
+    .containerIcones img{
+        width:100px;
+    }
+
+    @media(max-width:500px){
+        justify-content: center;
+        height: 80vh;
+        .meioVerde{
+            width: 90%;
+            height: fit-content;
+            border-radius: 0;
+            left: 0;
+        }
+        .cabecalho{
+            height: 150px;
+            flex-direction: column;
+        }
+        .cabecalho img{
+            width:30%;
+        }
+        .txtTitulo{
+            width:90%;
+            text-align: center;
+        }
+        .txtTitulo h1{
+            font-size: 3rem;
+        }
+        .txtTitulo h2{
+            font-size: 1rem;
+        }
+
+        .containerIcones{
+            width:100%;
+            height: fit-content;
+            justify-content: center;
+            margin-top: 10%;
+
+            border: solid 1px blue;
+        }
+        .cxCadaIcone{
+            width: 100px;
+            height: 100px;
+            margin-bottom:10%;
+        }
+        .containerIcones img{
+            width: 50px;
+        }
+        .cxCadaIcone p{
+            font-size:.7rem;
+        }
+
     }
    
-
-
-
-
 `
