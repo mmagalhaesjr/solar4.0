4d85cbd4a3e8baf496338fcd8bb40343efab8925
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-12-08 17:40:04 -0300

    responsividade 90%

--- src/componentes/LandingPages/Topo/Pagina3/Pagina3.jsx
+++ src/componentes/LandingPages/Topo/Pagina3/Pagina3.jsx
@@ -1,10 +1,83 @@
 import { StyledPagina3 } from "./styled";
 
-export default function Pagina3(){
-    return(
-        <StyledPagina3>
-            <h1>olo mundo</h1>
+import logo4 from '../../../../assets/logosIcones/logoBranca.png'
+import logo5 from '../../../../assets/logosIcones/intel-solar.png'
+import icone1 from '../../../../assets/landingPage/topoFunil/icone1.png'
+import icone2 from '../../../../assets/landingPage/topoFunil/icone2.png'
+import icone3 from '../../../../assets/landingPage/topoFunil/icone3.png'
+import icone4 from '../../../../assets/landingPage/topoFunil/icone4.png'
+import icone5 from '../../../../assets/landingPage/topoFunil/icone5.png'
+import icone6 from '../../../../assets/landingPage/topoFunil/icone6.png'
+import { FaLongArrowAltRight } from "react-icons/fa";
 
+export default function Pagina3() {
+    return (
+        <StyledPagina3>
+            <div className='cabecalho'>
+                <div className='subtitulo'>
+                    <h2>
+                        Seu dinheiro trabalhando
+                        para você
+                    </h2>
+                </div>
+                <div className='cximg'>
+                    <img src={logo4} alt="logo" />
+                    <img className='intel' src={logo5} alt="logo" />
+                </div>
+            </div>
+
+            <div className="container">
+
+                <div className="caixa">
+                    <div className="txt">
+                        <h3>Antes da energia Solar:</h3>
+                    </div>
+                    <div className="cx">
+
+                        <div className="divisaoCx">
+                            <section><img src={icone1} alt="icone"/></section>
+                            <section><p>Contas altas de luz</p></section>
+                        </div>
+                        <div className="divisaoCx">
+                            <section><img src={icone2} alt="icone"/></section>
+                            <section> <p>Gastos sem retorno </p></section>
+                        </div>
+                        <div className="divisaoCx">
+                            <section><img src={icone3} alt="icone"/></section>
+                            <section><p>Energia para um imóvel.</p></section>
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+                <FaLongArrowAltRight id="seta" />
+
+
+                <div className="caixa">
+                    <div className="txt">
+                        <h3>Depois da energia Solar:</h3>
+                    </div>
+                    <div className="cx">
+
+                        <div className="divisaoCx">
+                        <section> <img src={icone4} alt="icone" /></section>
+                        <section> <p>Contas que se pagam</p></section>
+                        </div>
+                        <div className="divisaoCx">
+                        <section> <img src={icone5} alt="icone" /></section>
+                        <section> <p>Gastos viram investimentos</p></section>
+                        </div>
+                        <div className="divisaoCx">
+                        <section> <img src={icone6} alt="icone" /></section>
+                        <section> <p>Energia para vários imóveis</p></section>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
         </StyledPagina3>
     )
 }
--- src/componentes/LandingPages/Topo/Pagina3/styled.js
+++ src/componentes/LandingPages/Topo/Pagina3/styled.js
@@ -2,17 +2,163 @@ import styled from "styled-components";
 
 import fot from '../../../../assets/landingPage/topoFunil/foto2.webp'
 
-
 const foto = `url(${fot})`;
 
 export const StyledPagina3 = styled.section`
 width: 100%;
-height: 100vh;
+
 
 background-image: ${foto};
 background-size: cover;
 background-repeat: no-repeat;
 background-position: 5% 70%;
+
+display: flex;
+flex-direction: column;
+align-items: center;
+
+
+
+.cabecalho{
+    width: 90%;
+    min-height: 200px;
+    margin-top: 3%;
+   
+    display: flex;
+    align-items: center;
+    justify-content:space-around;
+}
+.subtitulo{
+    min-height: 200px;
+    width: fit-content;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center; 
+}
+.subtitulo h2{
+    color: #ffffff;
+    font-size: 4rem;
+    font-weight: 600;
+    margin-top: 3%;
+}
+.cximg{
+    min-height: 200px;
+    width: fit-content;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    text-align: center;
+    gap: 5%;
+}
+.cximg img{
+    width: 200px;
+}
+.cximg .intel{
+    width: 150px;
+}
+
+
+
+.container{
+    width: 80%;
+    height: 500px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: solid 1px red;
+}
+.caixa{
+    width: 400px;
+    height: 400px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    border: solid 1px #ffffff;
+}
+.txt{
+    width: 90%;
+    color: #ffffff;
+    text-align: center;
+    margin-bottom: 3%;
+    font-size: 2rem;
+}
+.cx{
+    width: 90%;
+    min-width:350px ;
+    height: 90%;
+    border-radius: 5%;
+    background-color: #9b9999b9;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 5%;
+}
+.divisaoCx{
+    width: 95%;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    border: solid 1px red;
+}
+.divisaoCx section{
+    width: 100%;
+    height: 100%;
+    border: solid 1px orange;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+.divisaoCx img{
+    width: 100px;
+    height: 80px;
+
+}
+.divisaoCx p{
+    font-size: 1.5rem;
+    color: #ffffff;
+}
+
+#seta{
+    color: #ffffff;
+    font-size: 10rem;
+}
+
+@media( max-width:1100px) {
+    .container{
+        width: 95%;
+    }
+}
+@media( max-width:1100px) {
+    #seta{
+        display: none;
+    }
+}
+@media( max-width:830px) {
+    .container{
+        height: fit-content;
+        display: flex;
+        flex-direction:column;
+
+        margin-top: 5%;
+    }
+    .caixa{
+        
+    }
+}
 
 
 
