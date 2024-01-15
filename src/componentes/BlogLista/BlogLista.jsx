49e6e767b280ee48419e478531ad438b237121ee
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-01-15 15:03:10 -0300

    update

---
+++ src/componentes/BlogLista/BlogLista.jsx
@@ -0,0 +1,27 @@
+import foto1 from "../../assets/blogs/blog1/1.png"
+import foto2 from "../../assets/blogs/blog2/1.png"
+import foto3 from "../../assets/blogs/blog3/1.png"
+
+export const arrayBlogs = [
+    {
+        id: 3,
+        // link: '#/blog3',
+        img: foto3,
+        titulo: 'Crescimento da energia solar no Brasil e as implicações futuras',
+        texto: 'Hoje, o Brasil é um dos líderes em energia solar na América Latina e no mundo. Saiba os principais motivos por trás desse crescimento e os benefícios disponíveis.'
+    },
+    {
+        id: 2,
+        // link: '#/blog2',
+        img: foto2,
+        titulo: 'O futuro é solar: tendências e tecnologias',
+        texto: 'Neste blog, vamos explorar as tendências que estão moldando o futuro solar e impulsionando a transição para uma matriz energética mais sustentável.'
+    },
+    {
+        id: 1,
+        // link: '#/blog1',
+        img: foto1,
+        titulo: 'Conheça 5 vantagens de energia solar para empresas',
+        texto: 'Empresas de diferentes portes vêm investindo em Energia Solar como solução para se libertar das altas contas de energia elétrica.'
+    },
+];
--- src/pages/BlogTelainicial/BlogTelainicial.jsx
+++ src/pages/BlogTelainicial/BlogTelainicial.jsx
@@ -1,50 +1,16 @@
 import { StyledBlogPagina } from "./styled";
 
-
-
-// import BlogMaior from "../../componentes/BlogMaior/BlogMaior";
-import BlogMenor from '../../componentes/BlogMenor/BlogMenor';
-
-
-
-
 import { BsWhatsapp } from 'react-icons/bs';
 import Header2 from '../../componentes/Header2/Header2';
 import Footer from '../../componentes/Footer/Footer';
 
-
-import foto1 from "../../assets/blogs/blog1/1.png"
-import foto2 from "../../assets/blogs/blog2/1.png"
-import foto3 from "../../assets/blogs/blog3/1.png"
+// import BlogMaior from "../../componentes/BlogMaior/BlogMaior";
+import BlogMenor from '../../componentes/BlogMenor/BlogMenor';
+import {arrayBlogs} from '../../componentes/BlogLista/BlogLista'
 // import foto4 from "../../assets/blogs/blog4/1.png"
 
 
-
 export default function BlogTelainicial() {
-
-    const arrayBlogs = [
-        {
-            id: 3,
-            // link: '#/blog3',
-            img: foto3,
-            titulo: 'Crescimento da energia solar no Brasil e as implicações futuras',
-            texto: 'Hoje, o Brasil é um dos líderes em energia solar na América Latina e no mundo. Saiba os principais motivos por trás desse crescimento e os benefícios disponíveis.'
-        },
-        {
-            id: 2,
-            // link: '#/blog2',
-            img: foto2,
-            titulo: 'O futuro é solar: tendências e tecnologias',
-            texto: 'Neste blog, vamos explorar as tendências que estão moldando o futuro solar e impulsionando a transição para uma matriz energética mais sustentável.'
-        },
-        {
-            id: 1,
-            // link: '#/blog1',
-            img: foto1,
-            titulo: 'Conheça 5 vantagens de energia solar para empresas',
-            texto: 'Empresas de diferentes portes vêm investindo em Energia Solar como solução para se libertar das altas contas de energia elétrica.'
-        },
-    ];
 
 
     return (
