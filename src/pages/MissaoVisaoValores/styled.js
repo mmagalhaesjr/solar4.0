ea25735c48c7311aec5d772e0b12f5bf27f6852f
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-02-22 13:50:37 -0300

    update

--- src/pages/MissaoVisaoValores/styled.js
+++ src/pages/MissaoVisaoValores/styled.js
@@ -32,8 +32,13 @@ align-items: center;
  margin-bottom: 5%;
  margin-top: 5%;
 
-  background: rgba(255, 255, 255, 0.537); 
-  backdrop-filter: blur(10px); 
+
+ background-color: rgba(255, 255, 255, 0.537); 
+  -webkit-background-color: rgba(255, 255, 255, 0.537); 
+  
+  -webkit-backdrop-filter: blur(10px); 
+  backdrop-filter: blur(10px);
+
   box-shadow: 5px 5px 10px  rgba(0, 0, 0, 0.5);
   border-radius:10px ;
 
